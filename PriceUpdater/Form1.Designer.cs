646d5f7d7b470ef12c40c77b86423735cfff2dc5
Keith Miller <keithmiller00@gmail.com>
2018-10-22 16:52:34 +0700

    Add project files.

---
+++ PriceUpdater/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PriceUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_PCN_NO = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bSelectFile = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB_PCN_NO
+            // 
+            this.TB_PCN_NO.Location = new System.Drawing.Point(74, 118);
+            this.TB_PCN_NO.Name = "TB_PCN_NO";
+            this.TB_PCN_NO.Size = new System.Drawing.Size(179, 20);
+            this.TB_PCN_NO.TabIndex = 0;
+            this.TB_PCN_NO.Text = "045/J/VIII/2018";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PCN No";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Just UPDATE IT ALREADY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Select Price Update File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 325);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // bSelectFile
+            // 
+            this.bSelectFile.Location = new System.Drawing.Point(279, 321);
+            this.bSelectFile.Name = "bSelectFile";
+            this.bSelectFile.Size = new System.Drawing.Size(107, 23);
+            this.bSelectFile.TabIndex = 4;
+            this.bSelectFile.Text = "Browse";
+            this.bSelectFile.UseVisualStyleBackColor = true;
+            this.bSelectFile.Click += new System.EventHandler(this.bSelectFile_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 65);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Update From File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bSelectFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TB_PCN_NO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_PCN_NO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bSelectFile;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PriceUpdater/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Devart.Data.Oracle;
+using Dapper;
+using System.IO;
+
+namespace PriceUpdater
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.TB_PCN_NO.Text.Length == 0)
+                return;
+
+            string keyval = this.TB_PCN_NO.Text;
+            OracleManager om = new OracleManager("192.168.1.9");
+
+            IDbConnection conn = om.connect();
+
+            List<PCN_D> items = conn.Query<PCN_D>("Select * from PCN_D where PCN_NO ='" + keyval + "'").ToList();
+
+            foreach (PCN_D pc in items)
+            {
+                string sql = @"
+                        update item 
+                        set UNIT_RETAIL = " + pc.NEW_RETAIL + "," +
+                        "UPDATE_DATE = TO_DATE('" + DateTime.Now.ToString("MM/dd/yyyy")+"','MM/DD/YYYY'),"+
+                         "UPDATE_BY = 'AUTO' " +
+                         "WHERE ITEM_CODE = '" + pc.ITEM_CODE + "'";
+                try
+                {
+                    conn.Execute(sql);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                
+            }
+            MessageBox.Show(items.Count().ToString());
+            //Load all the records with a price change
+        }
+
+        private void bSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+
+                textBox1.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void UpdatePCN(List<ItemPrice> ip_list)
+        {
+            OracleManager om = new OracleManager("192.168.1.9");
+
+            IDbConnection conn = om.connect();
+            foreach (ItemPrice ip in ip_list)
+            {
+                string sql = @"
+                        update item
+                        set UNIT_RETAIL = " + ip.price + "," +
+                        "TXT=''," + 
+                        "UPDATE_DATE = TO_DATE('" + DateTime.Now.ToString("MM/dd/yyyy") + "','MM/DD/YYYY')," +
+                         "UPDATE_BY = 'AUTO' " +
+                         "WHERE ITEM_CODE = '" + ip.item_id + "'";
+                try
+                {
+                    conn.Execute(sql);
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+                return;
+
+            //Make sure file exists 
+            FileInfo fi = new FileInfo(textBox1.Text);
+
+            if (!fi.Exists)
+            {
+                MessageBox.Show("Cannot find the file yo.");
+                return;
+            }
+            FileStream fs = fi.OpenRead();
+            string buffer;
+            using (System.IO.StreamReader rdr = new System.IO.StreamReader(fs))
+            {
+                buffer = rdr.ReadToEnd();
+            }
+
+            List<ItemPrice> price_list = new List<ItemPrice>();
+            string[] lines = buffer.Split(
+                new[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (string a_line in lines)
+            {
+                if (a_line.Length == 0)
+                    continue;
+
+                string[] splits = a_line.Split(';');
+                ItemPrice an_item = new ItemPrice();
+                an_item.item_id = splits[0];
+                an_item.price = double.Parse(splits[1]);
+                price_list.Add(an_item);
+            }
+
+            UpdatePCN(price_list);
+        }
+    }
+
+    struct ItemPrice
+    {
+        public string item_id;
+        public double price;
+    }
+
+    class OracleManager
+    {
+        //public static IEnumerable<T> Query<T>(this IDbConnection cnn, string sql, object param = null, SqlTransaction transaction = null, bool buffered = true)
+
+        private string HostIP = "192.168.1.9";
+        public List<string> error_list = new List<string>();
+        public OracleManager(string HIP)
+        {
+            this.HostIP = HIP;
+        }
+
+        public OracleConnection connect()
+        {
+            OracleConnection oc = null;
+
+            //Build the key
+            OracleConnectionStringBuilder oraCSB = new OracleConnectionStringBuilder();
+            oraCSB.Direct = true;
+            oraCSB.Server = HostIP;
+            oraCSB.Port = 1521;
+            oraCSB.UserId = "erp";
+            oraCSB.Password = "erp";
+            oraCSB.ConnectionTimeout = 30;
+
+            oc = new OracleConnection(oraCSB.ConnectionString);
+
+
+            return oc;
+        }
+    }
+
+    public class PCN_D
+    {
+        public String PCN_NO { get; set; }
+        public DateTime PCN_DATE { get; set; }
+        public String ITEM_CODE { get; set; }
+        public Double NEW_COST { get; set; }
+        public Double NEW_RETAIL { get; set; }
+        public string PCN_REASON { get; set; }
+        public DateTime UPDATE_DATE { get; set; }
+        public String UPDATE_BY { get; set; }
+        public string TABLE_NAME = "PCN_D";
+    }
+
+}
